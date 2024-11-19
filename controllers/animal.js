2f11ef7e088b64b8be8ae0e5d0aacb160850a243
juliannamarshall <s553902@nwmissouri.edu>
2024-11-19 10:18:59 -0600

    resource for delete

--- controllers/animal.js
+++ controllers/animal.js
@@ -11,19 +11,19 @@ exports.animal_view_all_Page = async function(req, res){
 };
 
 exports.animal_detail = async function(req, res){
-    console.log("detail" + req.params.id);
+    console.log("detail" + req.params.species);
     try{
-        const result = await Animal.findById(req.params.id);
+        const result = await Animal.findOne({ species: req.params.species });
         if(result){
             res.send(result);
         }
         else{
             res.status(404);
-            res.send(`{"error": "Animal with id ${req.params.id} not found"}`);
+            res.send(`{"error": "Animal with id ${req.params.species} not found"}`);
         }
     } catch(err){
         res.status(500);
-        res.send(`{"error": "Document for id ${req.params.id} not found"}`);
+        res.send(`{"error": "Document for id ${req.params.species} not found"}`);
     }
 };
 
@@ -42,10 +42,48 @@ exports.animal_create_post = async function(req, res){
     }
 };
 
-exports.animal_delete = function(req, res){
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
+exports.animal_delete = async function(req, res){
+    console.log("request params: ", req.params);
+    console.log("delete " + req.params.species);
+    try{
+        result = await Animal.findOneAndDelete({ species: req.params.species });
+        console.log("Removed " + result);
+        res.send(result);
+    } catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
-exports.animal_update_put = function(req, res){
-    res.send('NOT IMPLEMENTED: Animal update PUT ' + req.params.id);
+exports.animal_update_put = async function(req, res){
+    console.log(`update on id ${req.params.species} with body ${JSON.stringify(req.body)}`);
+    try{
+        let toUpdate = await Animal.findOne({ species: req.params.species });
+        if(req.body.species){
+            toUpdate.species = req.body.species;
+        }
+        if(req.body.habitat){
+            toUpdate.habitat = req.body.habitat;
+        }
+        if(req.body.lifespan){
+            toUpdate.lifespan = req.body.lifespan;
+        }
+        let result = await toUpdate.save();
+        console.log("Success " + result);
+        res.send(result); 
+    } catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}: Update for id ${req.params.species} failed}`);
+    }
 };
+
+exports.animal_view_One_Page = async function(req, res){
+    console.log("Single view for id" + req.query.species);
+    try{
+        result = await Animal.findOne({ species: req.query.species });
+        res.render('animaldetail', {title: 'Animal Detail', toShow: result});
+    } catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+}
--- routes/resource.js
+++ routes/resource.js
@@ -6,9 +6,10 @@ var animal_controller = require('../controllers/animal');
 
 router.get('/', api_controller.api);
 router.post('/animal', animal_controller.animal_create_post);
-router.delete('/animal/:id', animal_controller.animal_delete);
-router.put('/animal/:id', animal_controller.animal_update_put);
-router.get('/animal/:id', animal_controller.animal_detail);
+router.delete('/animal/:species', animal_controller.animal_delete);
+router.put('/animal/:species', animal_controller.animal_update_put);
+router.get('/animal/:species', animal_controller.animal_detail);
 router.get('/animal', animal_controller.animal_view_all_Page);
+
 
 module.exports = router;
