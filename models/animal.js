be2434a684602c3cf93e35c56a5003654580abcb
juliannamarshall <s553902@nwmissouri.edu>
2024-12-03 10:43:44 -0600

    Part 6

--- models/animal.js
+++ models/animal.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const animalSchema = mongoose.Schema({
   species: { type: String, required: true },
   habitat: { type: String, required: true },
-  lifespan: { type: Number, required: true },
+  lifespan: { type: Number, required: true, min:[1], max: [150] },
 });
 
 module.exports = mongoose.model("Animal", animalSchema);
